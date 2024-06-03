a59f619448a0748995ed8ebf407631afea5d2401
Muhammad Risqi Firdaus <risqi.firdaus20@gmail.com>
2024-06-03 12:39:39 +0700

    rmove duplicates

---
+++ Easy/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        int n = 1;
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                nums[n] = nums[i];
+                n++;
+            }
+        }
+        return n;
+    }
+};
